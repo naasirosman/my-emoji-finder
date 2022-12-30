2bee020a173aa463fbd14bc3bf9e7201d4dd7293
Naasir Osman <109971732+naasirosman@users.noreply.github.com>
2022-12-30 10:21:03 +0000

    emojicards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-
 import './App.css';
-import Input from './Input';
-import EmojiCard from './EmojiCard';
+import { useState } from 'react';
+import emojis from './Emoji';
 
+const App = () => {
 
-function App() {
-  return (
-  <div className='card' onClick={() => document.querySelector('.card').style.width = '20rem'}>
-    <span className='search-emoji'>&#128269;</span>
-    <Input />
-
-  </div>
-
-  );
-}
+    const [myStyle, setMyStyle] = useState({});
+    const handleClick = (id) => {
+        setMyStyle((prevState) => ({
+            ...myStyle,
+            [id]: !prevState[id],
+        }));
+    };
+    return (
+        <div>
+            <div className="card">
+                <span className="search-emoji">&#128269;</span>
+                <input placeholder="Search for an Emoji!"></input>
+            </div>
+            <div>
+                {emojis.map((element, index) => {
+                    return (
+                        <div
+                            className="emoji-card"
+                            key={element.aliases}
+                            onClick={() => handleClick(index)}
+  
+                        >
+                            <span>{element.emoji}</span>
+                            <span
+                                key={element.unicode_version}
+                                className="description"
+                            >
+                                {element.description}
+                            </span>
+                            <div className='info
+                            '
+                                                      style={{
+                                                        display: myStyle[`${index}`]
+                                                            ? 'block'
+                                                            : 'initial',
+                                                    }}
+                            >
+                                <p className='expand'>unicode version: {element.unicode_version}</p>
+                                <p className='expand'>iOS version: {element.ios_version}</p>
+                                <p className='expand'>catergory: {element.category.toLowerCase()}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+                <i className="gg-chevron-double-down"></i>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Dropdown.js
@@ -0,0 +1,12 @@
+import React from "react";
+import emojis from "./Emoji";
+import EmojiCard from "./EmojiCard";
+import './App.css';
+
+const Dropdown = () => {
+    return (
+<div></div>
+    )
+}
+
+export default Dropdown
